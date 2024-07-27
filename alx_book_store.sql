40208d8c64ae62d8d6da4b62de818a40e13469f1
Mallifs <mallifs@icloud.com>
2024-07-27 23:50:11 +0300

    sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,24 +1,40 @@
+USE alx_book_store
+
+CREATE TABLE Authors(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Books(
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    Foreign Key (author_id) REFERENCES Authors(author_id)
+);
+
 CREATE TABLE Order_Details(
-    orderDetailID INT AUTO_INCREMENT PRIMARY KEY,
-    OrderID INT,
-    BookID INT,
-    Quantity DOUBLE,
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-    FOREIGN KEY (BookID) REFERENCES Books(BookID)
+    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+    Order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
 
 CREATE TABLE Orders(
-    OrderID INT AUTO_INCREMENT PRIMARY KEY,
-    CustomerID INT,
-    OrderDate DATE,
-    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 
 CREATE TABLE Customers(
-    CustomerID INT AUTO_INCREMENT PRIMARY KEY,
-    CustomerName VARCHAR(215) NOT NULL,
-    Email VARCHAR(215) NOT NULL,
-    Address TEXT
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
 );
