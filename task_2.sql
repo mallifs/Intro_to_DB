434a459c2cbd798811010ddc7c1b023de9000dd7
Mallifs <mallifs@icloud.com>
2024-07-28 08:24:54 +0300

    sql

--- task_2.sql
+++ task_2.sql
@@ -15,27 +15,27 @@ CREATE TABLE IF NOT EXISTS Books(
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-
-CREATE TABLE IF NOT EXISTS Customers(
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name  VARCHAR(215),
-    email VARCHAR(215),
-    address TEXT
+CREATE TABLE IF NOT EXISTS Customers (
+    CustomerID INT AUTO_INCREMENT PRIMARY KEY,
+    CustomerName VARCHAR(215) NOT NULL,
+    Email VARCHAR(215) NOT NULL,
+    Address TEXT
 );
 
 
-CREATE TABLE IF NOT EXISTS Orders(
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id  INT,
-    order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+CREATE TABLE IF NOT EXISTS Orders (
+    OrderID INT AUTO_INCREMENT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
 
-CREATE TABLE IF NOT EXISTS Order_Details(
-    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
+
+CREATE TABLE IF NOT EXISTS Order_Details (
+    OrderDetailID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT,
     book_id INT,
-    quantity DOUBLE
+    Quantity DOUBLE,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-
-
