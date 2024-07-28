bb71718e5a80bbcf7970197e51861ba018301e4d
Mallifs <mallifs@icloud.com>
2024-07-28 09:22:30 +0300

    task6_sql

---
+++ task_6.sql
@@ -0,0 +1,4 @@
+INSERT INTO customer(customer_id,customer_name,email,address)
+VALUES(2,'Blessing Malik','bmalik@sandtech.com','124 Happiness Ave.'),
+      (3,'Obed Ehoneah','eobed@sandtech@gmail.com','125 Hapiness Ave.'),
+      (4,'Nehemial Kamolu','nkamolu@sandtech.com','126 Hapiness Ave.');
