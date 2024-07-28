91aebd139a0dd977fe7de78a3e0ad908c529e150
Mallifs <mallifs@icloud.com>
2024-07-28 08:38:09 +0300

    show sql tables

---
+++ task_3.sql
@@ -0,0 +1,6 @@
+-- Write a script that list all the tables in alx_book_store in your MySQL server.
+
+-- The name of the file should be task_3.sql
+-- The database name will be passed as argument of mysql command
+
+SHOW TABLES;
